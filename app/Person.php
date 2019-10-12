f74ba0f36f9fcb08e027dba44312473460fcb670
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-12 13:44:18 +0100

    feat: add `Person` model

---
+++ app/Person.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model 
+{
+
+    protected $table = 'persons';
+    public $timestamps = true;
+
+    public function family()
+    {
+        return $this->belongsTo('Family');
+    }
+
+}
