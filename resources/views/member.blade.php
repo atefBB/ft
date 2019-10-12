a3c2d65200730f1518365c7916766676d142d357
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-12 14:18:09 +0100

    ui: display `members` data in `member` view

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -5,36 +5,55 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Members') }}</div>
-
+                <div class="card-header">
+                    {{ __('Members') }}
+                </div>
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
-
                     <div class="table-responsive">
-                      <table class="table table-hover">
-                        <thead>
-                            <tr>
-                              <th>#</th>
-                              <th>{{ __("First Name") }}</th>
-                              <th>{{ __("Last Name") }}</th>
-                              <th>{{ __("Birth Date") }}</th>
-                              <th>{{ __("Options") }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                              <th scope="row">1</th>
-                              <td>Mark</td>
-                              <td>Otto</td>
-                              <td>10-10-2010</td>
-                              <td></td>
-                            </tr>
-                        </tbody>
-                      </table>
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        #
+                                    </th>
+                                    <th>
+                                        {{ __("First Name") }}
+                                    </th>
+                                    <th>
+                                        {{ __("Last Name") }}
+                                    </th>
+                                    <th>
+                                        {{ __("Birth Date") }}
+                                    </th>
+                                    <th>
+                                        {{ __("Options") }}
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($members as $member)
+                                    <tr>
+                                        <th scope="row">
+                                            {{ $member->id }}
+                                        </th>
+                                        <td>{{ $member->firstname }}</td>
+                                        <td>{{ $member->lastname }}</td>
+                                        <td>{{ $member->birth_date }}</td>
+                                        <td>
+                                            <div aria-label="Options buttons" class="btn-group" role="group">
+                                                <button class="btn btn-success" type="button">{{ __("Edit") }}</button>
+                                                <button class="btn btn-danger" type="button">{{ __("Delete") }}</button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
