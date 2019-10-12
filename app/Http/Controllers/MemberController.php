67cb911b4c8a65b10da66b421a016a1c01b18617
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-12 17:44:02 +0100

    refactor: review `getting members` in `Member` controller

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Person;
 
 class MemberController extends Controller
 {
@@ -23,7 +24,7 @@ class MemberController extends Controller
      */
     public function index()
     {
-        $members = \App\Person::all();
+        $members = Person::all();
 
         return view(
             'member', array(
