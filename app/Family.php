b23a5bda746040978a3015c8e3923ae96c591dea
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-12 13:43:58 +0100

    feat: add `Family` model

---
+++ app/Family.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Family extends Model 
+{
+
+    protected $table = 'family';
+    public $timestamps = true;
+
+    public function members()
+    {
+        return $this->hasMany('Person');
+    }
+
+}
