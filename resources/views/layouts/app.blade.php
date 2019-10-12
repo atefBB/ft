eb0b9666a1f2afba6b635829206203d504389002
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-12 13:28:13 +0100

    ui: add `members/families` `dropdowns` items in `main right menu` in `home` page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/members">
+                                        {{ __('Members') }}
+                                    </a>
+                                    <a class="dropdown-item" href="/families">
+                                        {{ __('Families') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
