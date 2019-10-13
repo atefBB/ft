8205c90a7f2a475ea4e3c5362556f0afc80c0b1d
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-13 12:17:20 +0100

    ui: add `member family modal` in `members` table in `member` view

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -45,10 +45,13 @@
                                         <td>{{ $member->lastname }}</td>
                                         <td>{{ $member->birth_date }}</td>
                                         <td>
+                                            <b-modal id="f-modal-{{ $member->id }}" title="Family of {{ $member->firstname . " " . $member->lastname }}">
+                                                <p class="my-4">Hello from modal!</p>
+                                            </b-modal>
                                             <div aria-label="Options buttons" class="btn-group" role="group">
-                                                <button class="btn btn-info" type="button" v-b-tooltip title="{{ __('See Family') }}"><i class="far fa-eye"></i></button>
+                                                <button class="btn btn-info" type="button" v-b-tooltip title="{{ __('See Family') }}" v-b-modal.f-modal-{{ $member->id }}><i class="far fa-eye"></i></button>
                                                 <button class="btn btn-success" type="button" v-b-tooltip title="{{ __('Edit') }}"><i class="far fa-edit"></i></button>
-                                                <button class="btn btn-danger" type="button" v-b-tooltip title="{{__('Delete') }}"><i class="fas fa-times"></i></button>
+                                                <button class="btn btn-danger" type="button" v-b-tooltip title="{{ __('Delete') }}"><i class="fas fa-times"></i></button>
                                             </div>
                                         </td>
                                     </tr>
