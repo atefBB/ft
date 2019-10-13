8532d5bb1e7876c66dbfcdb2c8042a81b5d4327f
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-13 17:08:44 +0100

    style: clean-up `~/resources/js/app.js` file

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,7 +25,6 @@ Vue.use(BootstrapVue);
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component(
     'add-member-modal-component',
     require('./components/AddMemberModalComponent.vue').default
