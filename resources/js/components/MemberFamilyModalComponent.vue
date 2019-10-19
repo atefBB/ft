e1c50a6dbd1cf3c729a4f4e3abd736ec81c16e2e
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-19 16:33:15 +0100

    feat(frontend): add `member family modal` `vuejs` component

---
+++ resources/js/components/MemberFamilyModalComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-modal
+        :id="modal_id"
+        :title="model_title"
+        centered size="lg">
+        <p class="my-4">{{ member_family }}</p>
+        <template v-slot:modal-footer="{ ok, cancel, hide }">
+           <b-button variant="danger" @click="cancel()">
+             {{ i18n_cancel }}
+           </b-button>
+         </template>
+    </b-modal>
+</template>
+
+<script>
+    export default {
+        name: "member-family-modal",
+        props: {
+            modal_id: String,
+            model_title: String,
+            member_family: String,
+            i18n_cancel: String
+        }
+    }
+</script>
