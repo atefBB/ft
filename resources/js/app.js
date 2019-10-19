deb09064437b766213792b1b75d761b02a7487d1
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-19 16:34:22 +0100

    feat(frontend): add `member family modal` component to `main` js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,6 +30,11 @@ Vue.component(
     require('./components/AddMemberModalComponent.vue').default
 );
 
+Vue.component(
+    'member-family-modal-component',
+    require('./components/MemberFamilyModalComponent.vue').default
+);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
