515fae4cb5bbb56dc699c1a0f5fdc9da09c7a93a
Atef Ben Ali <atef.bettaib@gmail.com>
2019-10-21 22:31:55 +0100

    ui: `float-left` `main navbar dropdown` in `main app layout` file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,12 +34,10 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
+                    <ul class="navbar-nav mr-auto"></ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav float-left">
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
