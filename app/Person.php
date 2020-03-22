c163cd8781b9ef31d7052f19f87ea40bc615461d
Atef Ben Ali <atef.bettaib@gmail.com>
2020-03-22 14:59:21 +0100

    feat: add `fillable` propety in `person` model

--- app/Person.php
+++ app/Person.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model 
 {
-
+    protected $fillable = array(
+	"firstname", 
+	"lastname", 
+	"birth_date",
+        "gender"	
+    );
     protected $table = 'persons';
     public $timestamps = true;
-
+    
     public function family()
     {
         return $this->belongsTo('App\Family');
