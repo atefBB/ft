ea1e01bdedb432d24c681dd727baf070e122a1c6
Atef Ben Ali <atef.bettaib@gmail.com>
2020-03-22 15:12:22 +0100

    fix: fix `adding family member func` in `adding member modal component`

--- resources/js/components/AddMemberModalComponent.vue
+++ resources/js/components/AddMemberModalComponent.vue
@@ -2,29 +2,55 @@
     <b-modal id="add_member-modal" :title="model_title" centered size="lg">
         <b-container fluid>
             <b-row class="mb-1">
-              <b-col cols="3">{{ firstname }}</b-col>
-              <b-col>
-                <b-form-input id="firstname" :placeholder="firstname_placeholder" type="text" v-model="member.firstname"></b-form-input>
-              </b-col>
+                <b-col cols="3">{{ firstname }}</b-col>
+                <b-col>
+                    <b-form-input
+                        id="firstname"
+                        :placeholder="firstname_placeholder"
+                        type="text"
+                        v-model="member.firstname"
+                    ></b-form-input>
+                </b-col>
             </b-row>
             <b-row class="mb-1">
-              <b-col cols="3">{{ lastname }}</b-col>
-              <b-col>
-                <b-form-input id="lastname" :placeholder="lastname_placeholder" type="text" v-model="member.lastname"></b-form-input>
-              </b-col>
+                <b-col cols="3">{{ lastname }}</b-col>
+                <b-col>
+                    <b-form-input
+                        id="lastname"
+                        :placeholder="lastname_placeholder"
+                        type="text"
+                        v-model="member.lastname"
+                    ></b-form-input>
+                </b-col>
             </b-row>
             <b-row class="mb-1">
-              <b-col cols="3">{{ birth_date }}</b-col>
-              <b-col>
-                <b-form-input id="birth_date" v-model="member.birth_date" type="date"></b-form-input>
-              </b-col>
+                <b-col cols="3">{{ birth_date }}</b-col>
+                <b-col>
+                    <b-form-input
+                        id="birth_date"
+                        v-model="member.birth_date"
+                        type="date"
+                    ></b-form-input>
+                </b-col>
             </b-row>
             <b-row class="mb-1">
                 <b-col cols="3">{{ gender }}</b-col>
                 <b-col>
                     <b-form-group>
-                      <b-form-radio v-model="member.gender" model="selected" name="gender" value="male">{{ male }}</b-form-radio>
-                      <b-form-radio v-model="member.gender" model="selected" name="gender" value="female">{{ female }}</b-form-radio>
+                        <b-form-radio
+                            v-model="member.gender"
+                            model="selected"
+                            name="gender"
+                            value="male"
+                            >{{ male }}</b-form-radio
+                        >
+                        <b-form-radio
+                            v-model="member.gender"
+                            model="selected"
+                            name="gender"
+                            value="female"
+                            >{{ female }}</b-form-radio
+                        >
                     </b-form-group>
                 </b-col>
             </b-row>
@@ -37,27 +63,32 @@
 </template>
 
 <script>
-    export default {
-        data () {
-            return {
-                member: {}
-            }
-        },
-        props: {
-            model_title: String,
-            firstname: String,
-            lastname: String,
-            firstname_placeholder: String,
-            lastname_placeholder: String,
-            birth_date: String,
-            gender: String,
-            male: String,
-            female: String
-        },
-        methods: {
-            addMember () {
-                console.log(this.member);
-            }
+export default {
+    data() {
+        return {
+            member: {}
+        };
+    },
+    props: {
+        model_title: String,
+        firstname: String,
+        lastname: String,
+        firstname_placeholder: String,
+        lastname_placeholder: String,
+        birth_date: String,
+        gender: String,
+        male: String,
+        female: String
+    },
+    methods: {
+        addMember() {
+            axios
+                .put("/api/add/member", this.member)
+                .then(result => {
+		    this.$bvModal.hide("add_member-modal");
+                })
+                .catch(e => console.log(e.message));
         }
     }
+};
 </script>
